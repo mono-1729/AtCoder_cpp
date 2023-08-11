d7142067bb8cfd3d1270db2ebd1b27b78aaa49bc
mono-1729 <kawai.172957@gmail.com>
2023-08-12 05:45:52 +0900

    abc056

---
+++ ARC/056/b.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define rrep(i, a, n) for(int i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template<int MOD> struct ModInt {
+	static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+	ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	int get() const { return (int)x; }
+	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+	ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+	ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+	ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+	ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+		while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+		return ModInt(u); }
+	bool operator==(ModInt that) const { return x == that.x; }
+	bool operator!=(ModInt that) const { return x != that.x; }
+	ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+	ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
+
+int gcd(int a, int b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+vector<vector<int>> g(200005);
+
+// sを始点とする各頂点への距離の最小値を格納した配列を返す
+vector<int> dijk(int s){
+    priority_queue<pii, vector<pii>, greater<pii>> que;
+    vector<int> dist(200005, -1);
+    que.push(make_pair(-s, s));
+	dist[s] = s;
+    while(!que.empty()){
+        pii q = que.top(); que.pop();
+    	int d = -q.first, u = q.second;
+        for(auto nq: g[u]){
+            if(dist[nq] < min(nq,d)){
+                dist[nq] = min(nq,d);
+                que.push({-dist[nq], nq});
+            }
+        }
+    }
+    return dist;
+}
+
+int main() {
+	int n,m,s;cin>>n>>m>>s;
+	rep(i,0,m){
+		int u,v;cin>>u>>v;
+		u--;v--;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	vector<int>x=dijk(s-1);
+	rep(i,0,n){
+		if(x[i]>=i)cout<<i+1<<endl;
+	}
+	return 0;
+}
