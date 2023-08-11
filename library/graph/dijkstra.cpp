33a34e2caea281b25aafbef002e4a717c30b3177
mono-1729 <kawai.172957@gmail.com>
2023-08-12 05:44:00 +0900

    整備

---
+++ library/graph/dijkstra.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+const int MOD = 1000000007;
+const int mod = 998244353;
+
+vector<ll> a(100005);
+vector<vector<pll>> g(100005);
+
+// sを始点とする各頂点への距離の最小値を格納した配列を返す
+vector<ll> dijk(int s){
+    priority_queue<pll, vector<pll>, greater<pll>> que;
+    vector<ll> dist(100005, (1LL<<60));
+    que.push(make_pair(0, s));
+    dist[s] = 0;
+    while(!que.empty()){
+        pll q = que.top(); que.pop();
+        ll d = q.first, u = q.second;
+        if(dist[u] < d) continue;
+        for(auto nq: g[u]){
+            ll v = nq.first, cost = nq.second;
+            if(dist[v] > d+cost){
+                dist[v] = d+cost;
+                que.push({dist[v], v});
+            }
+        }
+    }
+    return dist;
+}
+
+int main(){
+    int n, m; cin >> n >> m;
+    rep(i, 0, m){
+        int u, v, c; cin >> u >> v >> c;
+        g[u].push_back({v, c});
+        g[v].push_back({u, c});
+    }
+
+    vector<ll> d = dijk(1);
+    rep(i, 1, n+1){
+        cout << d[i] << endl;
+    }
+    
+    return 0;
+}
