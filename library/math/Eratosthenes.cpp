b42727a8f0b2e6d56fde2ee8df5af448dd48296b
mono-1729 <kawai.172957@gmail.com>
2023-08-16 11:37:41 +0900

    abc206

--- library/math/Eratosthenes.cpp
+++ library/math/Eratosthenes.cpp
@@ -10,11 +10,13 @@ constexpr ll MOD = 998244353;
 constexpr int IINF = 1001001001;
 constexpr ll INF = 1LL<<60;
 // 0ならば素数, 0でないなら最⼩の素因数
-vector<int> erat(600001);
+// 素因数分解や素因数の数も求めることが可能
+int max_num=1000005;
+vector<int> erat(max_num);
 void init_e(){
-	rep(i, 2, sqrt(600000)){
+	rep(i, 2, max_num-1){
 		if(erat[i] == 0){
-			for(int j = i*i; j <= 600000; j += i){
+			for(int j = i*i; j <= max_num-1; j += i){
 				if(erat[j] == 0) erat[j] = i;
 			}
 		}
