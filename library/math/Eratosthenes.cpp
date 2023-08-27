a068de6347c22fc11a8b24e503ea3191fd556903
mono-1729 <kawai.172957@gmail.com>
2023-08-27 22:30:24 +0900

    arc116

--- library/math/Eratosthenes.cpp
+++ library/math/Eratosthenes.cpp
@@ -14,7 +14,7 @@ constexpr ll INF = 1LL<<60;
 int max_num=1000005;
 vector<int> erat(max_num);
 void init_e(){
-	rep(i, 2, max_num-1){
+	rep(i, 2, sqrt(max_num-1)){
 		if(erat[i] == 0){
 			for(int j = i*i; j <= max_num-1; j += i){
 				if(erat[j] == 0) erat[j] = i;
