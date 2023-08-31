36dcb6d45aa890fe2c3125d1985c4b5692a1b8ee
mono-1729 <kawai.172957@gmail.com>
2023-08-31 12:56:55 +0900

    abc294途中

---
+++ ABC/294/g.cpp
@@ -0,0 +1,295 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(int i = a; i < n; i++)
+#define rrep(i, a, n) for(int i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template<int MOD> struct ModInt {
+	static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+	ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	int get() const { return (int)x; }
+	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+	ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+	ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+	ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+	ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+		while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+		return ModInt(u); }
+	bool operator==(ModInt that) const { return x == that.x; }
+	bool operator!=(ModInt that) const { return x != that.x; }
+	ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+	ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
+
+int gcd(int a, int b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+template <class T, T (*op)(T, T), T (*e)(), class F, T (*mapping)(F, T), F (*composition)(F, F), F (*id)()> 
+class LazySegmentTree {
+    int _n, size, log;
+    vector<T> d;
+    vector<F> lz;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+
+    void all_apply(int k, F f){
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+
+    void push(int k){
+        all_apply(2*k, lz[k]);
+        all_apply(2*k+1, lz[k]);
+        lz[k] = id();
+    }
+
+public:
+    LazySegmentTree() : LazySegmentTree(0) {}
+    explicit LazySegmentTree(int n) : LazySegmentTree(vector<T>(n, e())) {} // explicit で明示的に型を指定する
+    explicit LazySegmentTree(const vector<T> &v) : _n(int(v.size())) {
+        // sizeは_nを超える最小の2のべき乗
+        size = 1;
+        while(size < _n) size *= 2, log++; 
+
+        // log は木の高さ（sizeの桁数）
+        log = 0;
+        while (!(size & (1 << log))) log++;
+
+        d = vector<T>(2*size, e());
+        lz = vector<F>(size, id());
+
+        for(int i = 0; i < _n; i++) d[size+i] = v[i];
+        for(int i = size-1; i >= 1; i--){
+            update(i);
+        }
+    }
+
+    void set(int p, T x){
+        assert(0 <= p && p < _n);
+        p += size;
+        for(int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for(int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    T get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for(int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+
+    T prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if(l == r) return e();
+
+        l += size;
+        r += size;
+
+        for(int i = log; i >= 1; i--){
+            if(((l >> i) << i) != l) push(l >> i);
+            if(((r >> i) << i) != r) push((r-1) >> i);
+        }
+        
+        T sml = e(), smr = e();
+        while(l < r){
+            if(l&1) sml = op(sml, d[l++]);
+            if(r&1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    T all_prod() {return d[1]; }
+
+    void apply(int p, F f){
+        assert(0 <= p && p < _n);
+        p += size;
+        for(int i = log; i >= 1; i--) push(p >> i); 
+        d[p] = mapping(f, d[p]);
+        for(int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    void apply(int l, int r, F f){
+        assert(0 <= l && l <= r && r <= _n);
+        if(l == r) return;
+
+        l += size;
+        r += size;
+
+        for(int i = log; i >= 1; i--){
+            if(((l >> i) << i) != l) push(l >> i);
+            if(((r >> i) << i) != r) push((r-1) >> i);
+        }
+
+        {
+            int l2 = l, r2 = r;
+            while(l < r){
+                if(l&1) all_apply(l++, f);
+                if(r&1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for(int i = 1; i <= log; i++){
+            if(((l >> i) << i) != l) update(l >> i);
+            if(((r >> i) << i) != r) update((r-1) >> i);
+        }
+    }
+
+    // f(op(a[l], a[l + 1], ..., a[r - 1])) = trueとなる最大のｒ
+    template <bool (*g)(T)> int max_right(int l) {
+        return max_right(l, [](T x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        T sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*g)(T)> int min_left(int r) {
+        return min_left(r, [](T x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        T sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+};
+
+using S=pii;
+ 
+S op(S a, S b) { 
+	if (a.first <= b.first) return a;
+	else return b; 
+}
+S e() { return {0, 0}; }
+// 一次関数 a x + b によって恒等写像と代入を表現
+using F = pii;
+S mapping(F a, S b) { 
+    if (a.first <= b.first) return a;
+	else return b; 
+}
+F composition(F a, F b){
+    if (a.first <= b.first) return a;
+	else return b; 
+}
+F id() { return {9e9,9e9}; }
+
+
+vector<vector<int>> G(200005);
+vector<vector<pii>> edges(200005);
+vector<pii> edge_to_index(200005),depth(400005,{0,0});
+vector<int> check(200005,0),depth(400005,0),dist(400005);
+
+int cnt=0;
+void dfs(int pos,int d){
+	check[pos]=1;
+    for(auto npos: edges[pos]){
+        if(check[npos.first] == 0) {
+			cnt++;
+			depth[cnt]={d+1,cnt};
+			dist[cnt]=G[npos.second][2];
+			edge_to_index[npos.second].first={cnt};
+			dfs(npos.first,d+1);
+			depth[cnt]={d,cnt};
+			dist[cnt]=-G[npos.second][2];
+			edge_to_index[npos.second].second={cnt};
+		}
+    }
+	cnt++;
+}
+
+int main() {
+	int n;cin>>n;
+	rep(i,0,n-1){
+		int u,v,w;cin>>u>>v>>w;
+		u--;v--;
+		G[i]={u,v,w};
+		edges[u].push_back({v,i});
+		edges[v].push_back({u,i});
+	}
+	dfs(0,0);
+	LazySegmentTree<S, op, e, F, mapping, composition, id> lst(depth);
+	BIT bit(2*n);
+
+	return 0;
+}
