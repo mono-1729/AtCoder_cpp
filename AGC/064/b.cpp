a54c6c78ce3846560ab12e182c37f3210e7c8d67
mono-1729 <kawai.172957@gmail.com>
2023-08-31 12:55:05 +0900

    agc064

---
+++ AGC/064/b.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template<int MOD> struct ModInt {
+	static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+	ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	int get() const { return (int)x; }
+	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+	ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+	ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+	ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+	ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+		while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+		return ModInt(u); }
+	bool operator==(ModInt that) const { return x == that.x; }
+	bool operator!=(ModInt that) const { return x != that.x; }
+	ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+	ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
+
+int gcd(int a, int b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+// ここから
+class UnionFind {
+    vector<ll> parent, maxi, mini;
+    inline ll root(ll n){
+        return (parent[n] < 0? n:parent[n] = root(parent[n]));
+    }
+public:
+    UnionFind(ll n_ = 1): parent(n_, -1), maxi(n_), mini(n_){
+        iota(maxi.begin(), maxi.end(), 0);
+        iota(mini.begin(), mini.end(), 0);
+    }
+
+    inline bool same(ll x, ll y){
+        return root(x) == root(y);
+    }
+
+    inline void unite(ll x, ll y){
+        ll rx = root(x);
+        ll ry = root(y);
+        if(rx == ry) return;
+        if(parent[rx] > parent[ry]) swap(rx, ry);
+        parent[rx] += parent[ry];
+        parent[ry] = rx;
+        maxi[x] = std::max(maxi[x],maxi[y]);
+        mini[x] = std::min(mini[x],mini[y]);
+    }
+
+    inline ll min(ll x){
+        return mini[root(x)];
+    }
+
+    inline ll max(int x){
+        return mini[root(x)];
+    }
+
+    inline ll size(ll x){
+        return (-parent[root(x)]);
+    }
+
+    inline ll operator[](ll x){
+        return root(x);
+    }
+
+    inline void print(){
+        rep(i, 0, (ll)parent.size()) cout << root(i) << " ";
+        cout << endl;
+    }
+
+    void clear(){
+        rep(i, 0, (ll)parent.size()){
+            parent[i] = -1;
+        }
+        iota(maxi.begin(), maxi.end(), 0);
+        iota(mini.begin(), mini.end(), 0);
+    }
+};
+
+int main() {
+	int n,m;cin>>n>>m;
+	vector<vector<pii>>g(n);
+	vector<pii>v;
+	vector<char>color;
+	rep(i,0,m){
+		int a,b;char c;cin>>a>>b>>c;
+		a--;b--;
+		g[a].push_back({b,i});
+		g[b].push_back({a,i});
+		v.push_back({a,b});
+		color.push_back(c);
+	}
+	string s;cin>>s;
+	UnionFind uf(n);
+	queue<int>q;
+	vector<int>ans;
+	set<int>st;
+	rep(i,0,m){
+		int a=v[i].first,b=v[i].second;
+		if(color[i]!=s[a]||color[i]!=s[b]){
+			continue;
+		}
+		if(st.find(a)!=st.end()||st.find(b)!=st.end()){
+			continue;
+		}
+		uf.unite(a,b);
+		st.insert(a);
+		st.insert(b);
+		ans.push_back(i);
+	}
+	for(auto num:st) q.push(num);
+	while(!q.empty()){
+		int now=q.front();q.pop();
+		for(auto next:g[now]){
+			if(st.find(next.first)!=st.end()) continue;
+			if(color[next.second]!=s[next.first]) continue;
+			uf.unite(now,next.first);
+			ans.push_back(next.second);
+			st.insert(next.first);
+			q.push(next.first);
+		}
+	}
+	if(st.size()!=n){
+		cout<<"No"<<endl;
+		return 0;
+	}
+	cout<<"Yes"<<endl;
+	rep(i,0,m){
+		int a=v[i].first,b=v[i].second;
+		if(uf.same(a,b)){
+			continue;
+		}
+		uf.unite(a,b);
+		st.insert(a);
+		st.insert(b);
+		ans.push_back(i);
+	}
+	rep(i,0,n-1)cout<<ans[i]+1<<" ";
+	cout<<endl;
+	return 0;
+}
