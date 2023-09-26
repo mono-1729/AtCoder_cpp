efa94d217471aa468a715670fbd7c611e786b680
mono-1729 <kawai.172957@gmail.com>
2023-09-26 09:13:41 +0900

    20230926

---
+++ ABC/242/g.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template<int MOD> struct ModInt {
+	static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+	ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	int get() const { return (int)x; }
+	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+	ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+	ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+	ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+	ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+		while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+		return ModInt(u); }
+	bool operator==(ModInt that) const { return x == that.x; }
+	bool operator!=(ModInt that) const { return x != that.x; }
+	ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+	ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
+
+int gcd(int a, int b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+struct Mo {
+  int n;
+  vector< pair< int, int > > lr;
+
+  explicit Mo(int n) : n(n) {}
+
+  void add(int l, int r) { /* [l, r) */
+    lr.emplace_back(l, r);
+  }
+
+  template< typename AL, typename AR, typename EL, typename ER, typename O >
+  void build(const AL &add_left, const AR &add_right, const EL &erase_left, const ER &erase_right, const O &out) {
+    int q = (int) lr.size();
+    int bs = n / min< int >(n, sqrt(q));
+    vector< int > ord(q);
+    iota(begin(ord), end(ord), 0);
+    sort(begin(ord), end(ord), [&](int a, int b) {
+      int ablock = lr[a].first / bs, bblock = lr[b].first / bs;
+      if(ablock != bblock) return ablock < bblock;
+      return (ablock & 1) ? lr[a].second > lr[b].second : lr[a].second < lr[b].second;
+    });
+    int l = 0, r = 0;
+    for(auto idx : ord) {
+      while(l > lr[idx].first) add_left(--l);
+      while(r < lr[idx].second) add_right(r++);
+      while(l < lr[idx].first) erase_left(l++);
+      while(r > lr[idx].second) erase_right(--r);
+      out(idx);
+    }
+  }
+
+  template< typename A, typename E, typename O >
+  void build(const A &add, const E &erase, const O &out) {
+    build(add, add, erase, erase, out);
+  }
+};
+
+int main() {
+	ll n;cin>>n;
+	vector<ll> a(n);
+	rep(i, 0, n) cin>>a[i];
+	ll q;cin>>q;
+	Mo mo(n);
+	rep(i,0,q){
+		ll l, r;cin>>l>>r;
+		mo.add(l-1, r);
+	}
+	vector<ll> cnt(n+1),ans(q);
+	int sum=0;
+	auto add = [&](int i) {
+		if(cnt[a[i]]++%2 == 1) ++sum;
+	};
+	auto erase = [&](int i) {
+		if(--cnt[a[i]]%2 == 1) --sum;
+	};
+	auto out = [&](int q) {
+		ans[q] = sum;
+	};
+	mo.build(add, erase, out);
+	for(auto &p: ans) cout << p << "\n";
+	return 0;
+}
