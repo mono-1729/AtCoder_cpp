54ed81b8b837c8ad0298bb83e1f62906f964d03a
mono-1729 <kawai.172957@gmail.com>
2023-09-29 00:08:43 +0900

    20230928

---
+++ ABC/255/f.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template<int MOD> struct ModInt {
+	static const int Mod = MOD; unsigned x; ModInt() : x(0) { }
+	ModInt(signed sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	ModInt(signed long long sig) { x = sig < 0 ? sig % MOD + MOD : sig % MOD; }
+	int get() const { return (int)x; }
+	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+	ModInt &operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
+	ModInt &operator/=(ModInt that) { return *this *= that.inverse(); }
+	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+	ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
+	ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+	ModInt inverse() const { long long a = x, b = MOD, u = 1, v = 0;
+		while (b) { long long t = a / b; a -= t * b; std::swap(a, b); u -= t * v; std::swap(u, v); }
+		return ModInt(u); }
+	bool operator==(ModInt that) const { return x == that.x; }
+	bool operator!=(ModInt that) const { return x != that.x; }
+	ModInt operator-() const { ModInt t; t.x = x == 0 ? 0 : Mod - x; return t; }
+};
+template<int MOD> ostream& operator<<(ostream& st, const ModInt<MOD> a) { st << a.get(); return st; };
+template<int MOD> ModInt<MOD> operator^(ModInt<MOD> a, unsigned long long k) {
+	ModInt<MOD> r = 1; while (k) { if (k & 1) r *= a; a *= a; k >>= 1; } return r; }
+typedef ModInt<998244353> mint;
+
+int gcd(int a, int b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+int n;
+ll s[200001],t[200001],tin[200001];
+pll ans[200001];
+bool solve(int l1,int r1,int l2,int r2){
+	int index=tin[s[l1]];
+	if(index<l2 || r2<index)return false;
+	if(index!=l2){
+		ans[s[l1]].first=s[l1+1];
+		if(!solve(l1+1,l1+(index-l2),l2,index-1)) return false;
+	}
+	if(index!=r2){
+		ans[s[l1]].second=s[l1+1+(index-l2)];
+		if(!solve(l1+1+(index-l2),r1,index+1,r2)) return false;
+	}
+	return true;
+}
+
+
+
+int main() {
+	cin>>n;
+	rep(i,0,n)cin>>s[i];
+	rep(i,0,n)cin>>t[i];
+	rep(i,0,n)tin[t[i]]=i;
+	if(s[0]!=1 || solve(0,n-1,0,n-1)==false){
+		cout<<-1<<endl;
+		return 0;
+	}
+	rep(i,1,n+1) cout<<ans[i].first<<" "<<ans[i].second<<endl;
+	return 0;
+}
