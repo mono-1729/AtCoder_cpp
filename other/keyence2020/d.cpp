428319ebfea032f726376235e50df3801e4eddf1
mono-1729 <kawai.172957@gmail.com>
2023-10-09 11:35:07 +0900

    20231009

---
+++ other/keyence2020/d.cpp
@@ -0,0 +1,176 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+	ll val;
+	static vector<modint<MOD>> factorial_vec;
+public:
+	// コンストラクタ
+	modint(ll x = 0){
+		val = x % MOD;
+		if(val < 0) x += MOD;
+	}
+
+	// 入出力ストリーム
+	friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+		ll y; is >> y;
+		x = y;
+		return is;
+	}
+	friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+		return os << x.val;
+	}
+
+	// 算術演算子
+	modint<MOD> operator -(){return modint<MOD>(-val);}
+	modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+	modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+	modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+	modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+	// 代入演算子
+	modint<MOD> &operator +=(const modint<MOD> &r){
+		val += r.val;
+		if(val >= MOD) val -= MOD;
+		return *this;
+	}
+	modint<MOD> &operator -=(const modint<MOD> &r){
+		if(val < r.val) val += MOD;
+		val -= r.val;
+		return *this;
+	}
+	modint<MOD> &operator *=(const modint<MOD> &r){
+		val = val*r.val%MOD;
+		if(val < 0) val += MOD;
+		return *this;
+	}
+	modint<MOD> &operator /=(const modint<MOD> &r){
+		*this *= inv(r);
+		return *this;
+	}
+
+	//等価比較演算子
+	bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+	bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+	bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+	bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+	bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+	bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+	// 累乗
+	static modint<MOD> modpow(modint<MOD> num, ll exp){
+		if(!exp) return modint<MOD>(1); // 0乗
+		modint<MOD> ret(1);
+		modint<MOD> tmp = num;
+		while(exp){
+			if(exp&1) ret *= tmp;
+			tmp *= tmp;
+			exp >>= 1;
+		}
+		return ret;
+	}
+
+	// 逆元
+	static modint<MOD> inv(modint<MOD> num){
+		return modpow(num, MOD-2);
+	}
+
+	// 階乗
+	static modint<MOD> factorial(ll n){
+		modint<MOD> ret(1);
+		if(n == 0) return ret;
+		if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+		ret = factorial(n-1)*n;
+		factorial_vec.push_back(ret);
+		return ret;
+	}
+
+	// コンビネーション
+	static modint<MOD> combination(ll n, ll r){
+		return factorial(n) / factorial(r) / factorial(n-r);
+	}
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+int gcd(int a, int b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+int lcm(int a, int b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+int main() {
+	ll n;cin>>n;
+	vector<int>a(n),b(n);
+	rep(i,0,n)cin>>a[i];
+	rep(i,0,n)cin>>b[i];
+	ll ans=INF;
+	rep(i,0,powMod(2,n)){
+		vector<pll>even,odd;
+		ll tmp=i;
+		ll cnt=0;
+		rep(j,0,n){
+			if((tmp>>j)&1){
+				if(j%2==0)even.push_back({a[j],j});
+				else odd.push_back({a[j],j});
+			}else{
+				if(j%2==0)odd.push_back({b[j],j});
+				else even.push_back({b[j],j});
+				cnt++;
+			}
+		}
+		if(odd.size()!=n/2)continue;
+		if(cnt%2==1)continue;
+		sort(even.begin(),even.end());
+		sort(odd.begin(),odd.end());
+		vector<pll>sorted;
+		rep(j,0,n){
+			if(j%2==0)sorted.push_back(even[j/2]);
+			else sorted.push_back(odd[j/2]);
+		}
+		bool ok=true;
+		rep(j,0,n-1){
+			if(sorted[j].first>sorted[j+1].first)ok=false;
+		}
+		if(!ok)continue;
+		ll num=0;
+		rep(j,0,n){
+			rep(k,j+1,n){
+				if(sorted[j].second>sorted[k].second)num++;
+			}
+		}
+		//cout<<i<<" "<<num<<endl;
+		chmin(ans,num);
+	}
+	if(ans>IINF)cout<<-1<<endl;
+	else cout<<ans<<endl;
+	return 0;
+}
