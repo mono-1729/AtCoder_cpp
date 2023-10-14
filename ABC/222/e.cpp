e5f358ad8f1ced7413f0b13b1ea0f49472972f2d
mono-1729 <kawai.172957@gmail.com>
2023-10-14 20:06:37 +0900

    20231014

---
+++ ABC/222/e.cpp
@@ -0,0 +1,187 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+	ll val;
+	static vector<modint<MOD>> factorial_vec;
+public:
+	// コンストラクタ
+	modint(ll x = 0){
+		val = x % MOD;
+		if(val < 0) x += MOD;
+	}
+
+	// 入出力ストリーム
+	friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+		ll y; is >> y;
+		x = y;
+		return is;
+	}
+	friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+		return os << x.val;
+	}
+
+	// 算術演算子
+	modint<MOD> operator -(){return modint<MOD>(-val);}
+	modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+	modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+	modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+	modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+	// 代入演算子
+	modint<MOD> &operator +=(const modint<MOD> &r){
+		val += r.val;
+		if(val >= MOD) val -= MOD;
+		return *this;
+	}
+	modint<MOD> &operator -=(const modint<MOD> &r){
+		if(val < r.val) val += MOD;
+		val -= r.val;
+		return *this;
+	}
+	modint<MOD> &operator *=(const modint<MOD> &r){
+		val = val*r.val%MOD;
+		if(val < 0) val += MOD;
+		return *this;
+	}
+	modint<MOD> &operator /=(const modint<MOD> &r){
+		*this *= inv(r);
+		return *this;
+	}
+
+	//等価比較演算子
+	bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+	bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+	bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+	bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+	bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+	bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+	// 累乗
+	static modint<MOD> modpow(modint<MOD> num, ll exp){
+		if(!exp) return modint<MOD>(1); // 0乗
+		modint<MOD> ret(1);
+		modint<MOD> tmp = num;
+		while(exp){
+			if(exp&1) ret *= tmp;
+			tmp *= tmp;
+			exp >>= 1;
+		}
+		return ret;
+	}
+
+	// 逆元
+	static modint<MOD> inv(modint<MOD> num){
+		return modpow(num, MOD-2);
+	}
+
+	// 階乗
+	static modint<MOD> factorial(ll n){
+		modint<MOD> ret(1);
+		if(n == 0) return ret;
+		if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+		ret = factorial(n-1)*n;
+		factorial_vec.push_back(ret);
+		return ret;
+	}
+
+	// コンビネーション
+	static modint<MOD> combination(ll n, ll r){
+		return factorial(n) / factorial(r) / factorial(n-r);
+	}
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+ll gcd(ll a, ll b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+ll lcm(ll a, ll b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+int main() {
+	ll n,m,k;cin>>n>>m>>k;
+	vector<ll>a(m);
+	rep(i,0,m)cin>>a[i];
+	vector<vector<pll>>g(n+1);
+	rep(i,0,n-1){
+		ll u,v;cin>>u>>v;
+		g[u].push_back({v,i});
+		g[v].push_back({u,i});
+	}
+	vector<ll>count(n,0);
+	rep(i,1,m){
+		ll s=a[i-1],t=a[i];
+		queue<vector<ll>>q;
+		q.push({s});
+		vector<ll>dist(n+1,-1);
+		dist[s]=0;
+		while(dist[t]==-1){
+			vector<ll>now=q.front();q.pop();
+			ll v=now[0];
+			for(auto tmp:g[v]){
+				ll nv=tmp.first,index=tmp.second;
+				if(dist[nv]==-1){
+					dist[nv]=dist[v]+1;
+					vector<ll>next=now;
+					next[0]=nv;
+					next.push_back(index);
+					if(nv==t){
+						rep(x,1,next.size()){
+							count[next[x]]++;
+						}
+						break;
+					}
+					q.push(next);
+				}
+			}
+		}
+	}
+	vector<ll>dp(400000);
+	dp[200000]=1;
+	rep(i,0,n-1){
+		vector<ll>next(400000);
+		rep(j,0,400000){
+			if(dp[j]==0)continue;
+			if(j-count[i]>=100000&&j-count[i]<=300000){
+				next[j-count[i]]+=dp[j];
+				next[j-count[i]]%=MOD;
+			}
+			if(j+count[i]>=100000&&j+count[i]<=300000){
+				next[j+count[i]]+=dp[j];
+				next[j+count[i]]%=MOD;
+			}
+		}
+		dp=next;
+	}
+	cout<<dp[200000+k]<<endl;
+	return 0;
+}
