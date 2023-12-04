8cf137e56aad50e07b4454336ced9e02bc9f933a
mono-1729 <kawai.172957@gmail.com>
2023-12-04 23:19:29 +0900

    20231204

---
+++ other/dp/r.cpp
@@ -0,0 +1,271 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll MOD = 1000000007;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+	ll val;
+	static vector<modint<MOD>> factorial_vec;
+public:
+	ll get() const { return (ll)val; }
+	// コンストラクタ
+	modint(ll x = 0){
+		val = x % MOD;
+		if(val < 0) x += MOD;
+	}
+
+	// 入出力ストリーム
+	friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+		ll y; is >> y;
+		x = y;
+		return is;
+	}
+	friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+		return os << x.val;
+	}
+
+	// 算術演算子
+	modint<MOD> operator -(){return modint<MOD>(-val);}
+	modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+	modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+	modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+	modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+	// 代入演算子
+	modint<MOD> &operator +=(const modint<MOD> &r){
+		val += r.val;
+		if(val >= MOD) val -= MOD;
+		return *this;
+	}
+	modint<MOD> &operator -=(const modint<MOD> &r){
+		if(val < r.val) val += MOD;
+		val -= r.val;
+		return *this;
+	}
+	modint<MOD> &operator *=(const modint<MOD> &r){
+		val = val*r.val%MOD;
+		if(val < 0) val += MOD;
+		return *this;
+	}
+	modint<MOD> &operator /=(const modint<MOD> &r){
+		*this *= inv(r);
+		return *this;
+	}
+
+	//等価比較演算子
+	bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+	bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+	bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+	bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+	bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+	bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+	// 累乗
+	static modint<MOD> modpow(modint<MOD> num, ll exp){
+		if(!exp) return modint<MOD>(1); // 0乗
+		modint<MOD> ret(1);
+		modint<MOD> tmp = num;
+		while(exp){
+			if(exp&1) ret *= tmp;
+			tmp *= tmp;
+			exp >>= 1;
+		}
+		return ret;
+	}
+
+	// 逆元
+	static modint<MOD> inv(modint<MOD> num){
+		return modpow(num, MOD-2);
+	}
+
+	// 階乗
+	static modint<MOD> factorial(ll n){
+		modint<MOD> ret(1);
+		if(n == 0) return ret;
+		if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+		ret = factorial(n-1)*n;
+		factorial_vec.push_back(ret);
+		return ret;
+	}
+
+	// コンビネーション
+	static modint<MOD> combination(ll n, ll r){
+		return factorial(n) / factorial(r) / factorial(n-r);
+	}
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+ll gcd(ll a, ll b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+ll lcm(ll a, ll b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+template< class T >
+struct Matrix {
+  vector< vector< T > > A;
+
+  Matrix() {}
+
+  Matrix(size_t n, size_t m) : A(n, vector< T >(m, 0)) {}
+
+  Matrix(size_t n) : A(n, vector< T >(n, 0)) {};
+
+  size_t height() const {
+    return (A.size());
+  }
+
+  size_t width() const {
+    return (A[0].size());
+  }
+
+  inline const vector< T > &operator[](ll k) const {
+    return (A.at(k));
+  }
+
+  inline vector< T > &operator[](ll k) {
+    return (A.at(k));
+  }
+
+  static Matrix I(size_t n) {
+    Matrix mat(n);
+    for(ll i = 0; i < n; i++) mat[i][i] = 1;
+    return (mat);
+  }
+
+  Matrix &operator+=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for(ll i = 0; i < n; i++)
+      for(ll j = 0; j < m; j++)
+        (*this)[i][j] += B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator-=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for(ll i = 0; i < n; i++)
+      for(ll j = 0; j < m; j++)
+        (*this)[i][j] -= B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator*=(const Matrix &B) {
+    size_t n = height(), m = B.width(), p = width();
+    assert(p == B.height());
+    vector< vector< T > > C(n, vector< T >(m, 0));
+    for(ll i = 0; i < n; i++)
+      for(ll j = 0; j < m; j++)
+        for(ll k = 0; k < p; k++)
+          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+    A.swap(C);
+    return (*this);
+  }
+
+  Matrix &operator^=(long long k) {
+    Matrix B = Matrix::I(height());
+    while(k > 0) {
+      if(k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    A.swap(B.A);
+    return (*this);
+  }
+
+  Matrix operator+(const Matrix &B) const {
+    return (Matrix(*this) += B);
+  }
+
+  Matrix operator-(const Matrix &B) const {
+    return (Matrix(*this) -= B);
+  }
+
+  Matrix operator*(const Matrix &B) const {
+    return (Matrix(*this) *= B);
+  }
+
+  Matrix operator^(const long long k) const {
+    return (Matrix(*this) ^= k);
+  }
+
+  friend ostream &operator<<(ostream &os, Matrix &p) {
+    size_t n = p.height(), m = p.width();
+    for(ll i = 0; i < n; i++) {
+      os << "[";
+      for(ll j = 0; j < m; j++) {
+        os << p[i][j] << (j + 1 == m ? "]\n" : ",");
+      }
+    }
+    return (os);
+  }
+
+
+  T determinant() {
+    Matrix B(*this);
+    assert(width() == height());
+    T ret = 1;
+    for(ll i = 0; i < width(); i++) {
+      ll idx = -1;
+      for(ll j = i; j < width(); j++) {
+        if(B[j][i] != 0) idx = j;
+      }
+      if(idx == -1) return (0);
+      if(i != idx) {
+        ret *= -1;
+        swap(B[i], B[idx]);
+      }
+      ret *= B[i][i];
+      T vv = B[i][i];
+      for(ll j = 0; j < width(); j++) {
+        B[i][j] /= vv;
+      }
+      for(ll j = i + 1; j < width(); j++) {
+        T a = B[j][i];
+        for(ll k = 0; k < width(); k++) {
+          B[j][k] -= B[i][k] * a;
+        }
+      }
+    }
+    return (ret);
+  }
+};
+
+int main() {
+	ll n,k;cin>>n>>k;
+	Matrix<mint> ans(1,n),dp(n,n);
+	rep(i,0,n) ans[0][i]=1;
+	rep(i,0,n)rep(j,0,n) cin>>dp[i][j];
+	ans*=dp^k;
+	mint sum=0;
+	rep(i,0,n)sum+=ans[0][i];
+	cout<<sum<<endl;
+	return 0;
+}
