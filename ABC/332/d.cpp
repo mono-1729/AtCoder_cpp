0213526226214812bbb1131f67cd5e1157d0b0b9
mono-1729 <kawai.172957@gmail.com>
2023-12-10 23:50:09 +0900

    20231210

---
+++ ABC/332/d.cpp
@@ -0,0 +1,172 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+constexpr ll mod = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+	ll val;
+	static vector<modint<MOD>> factorial_vec;
+public:
+	ll get() const { return (ll)val; }
+	// コンストラクタ
+	modint(ll x = 0){
+		val = x % MOD;
+		if(val < 0) x += MOD;
+	}
+
+	// 入出力ストリーム
+	friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+		ll y; is >> y;
+		x = y;
+		return is;
+	}
+	friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+		return os << x.val;
+	}
+
+	// 算術演算子
+	modint<MOD> operator -(){return modint<MOD>(-val);}
+	modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+	modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+	modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+	modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+	// 代入演算子
+	modint<MOD> &operator +=(const modint<MOD> &r){
+		val += r.val;
+		if(val >= MOD) val -= MOD;
+		return *this;
+	}
+	modint<MOD> &operator -=(const modint<MOD> &r){
+		if(val < r.val) val += MOD;
+		val -= r.val;
+		return *this;
+	}
+	modint<MOD> &operator *=(const modint<MOD> &r){
+		val = val*r.val%MOD;
+		if(val < 0) val += MOD;
+		return *this;
+	}
+	modint<MOD> &operator /=(const modint<MOD> &r){
+		*this *= inv(r);
+		return *this;
+	}
+
+	//等価比較演算子
+	bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+	bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+	bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+	bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+	bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+	bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+	// 累乗
+	static modint<MOD> modpow(modint<MOD> num, ll exp){
+		if(!exp) return modint<MOD>(1); // 0乗
+		modint<MOD> ret(1);
+		modint<MOD> tmp = num;
+		while(exp){
+			if(exp&1) ret *= tmp;
+			tmp *= tmp;
+			exp >>= 1;
+		}
+		return ret;
+	}
+
+	// 逆元
+	static modint<MOD> inv(modint<MOD> num){
+		return modpow(num, MOD-2);
+	}
+
+	// 階乗
+	static modint<MOD> factorial(ll n){
+		modint<MOD> ret(1);
+		if(n == 0) return ret;
+		if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+		ret = factorial(n-1)*n;
+		factorial_vec.push_back(ret);
+		return ret;
+	}
+
+	// コンビネーション
+	static modint<MOD> combination(ll n, ll r){
+		return factorial(n) / factorial(r) / factorial(n-r);
+	}
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+ll gcd(ll a, ll b){
+	if(a%b == 0){
+	  return b;
+	}else{
+	  return gcd(b, a%b);
+	}
+}
+
+ll lcm(ll a, ll b){
+	return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+	if (n == 0) return 1 % MOD;
+	ll val = powMod(x, n / 2);
+	val *= val;
+	val %= MOD;
+	if (n % 2 == 1) val *= x;
+	return val % MOD;
+}
+
+int main() {
+	ll h,w;cin>>h>>w;
+	vector<vector<ll>>a(h,vector<ll>(w)),b(h,vector<ll>(w));
+	rep(i,0,h)rep(j,0,w)cin>>a[i][j];
+	rep(i,0,h)rep(j,0,w)cin>>b[i][j];
+	vector<ll> tate,yoko;
+	rep(i,0,h)tate.push_back(i);
+	rep(i,0,w)yoko.push_back(i);
+	ll ans=INF;
+	do{
+		do{
+			bool flag=true;
+			rep(i,0,h)rep(j,0,w){
+				if(b[i][j]!=a[tate[i]][yoko[j]]) flag=false;
+			}
+			if(flag){
+				ll cnt=0;
+				vector<ll> tate2=tate,yoko2=yoko;
+				rep(i,0,h){
+					rrep(j,h-1,i+1){
+						if(tate2[j]==i){
+							swap(tate2[j],tate2[j-1]);
+							cnt++;
+						}
+					}
+				}
+				rep(i,0,w){
+					rrep(j,w-1,i+1){
+						if(yoko2[j]==i){
+							swap(yoko2[j],yoko2[j-1]);
+							cnt++;
+						}
+					}
+				}
+				chmin(ans,cnt);
+			}
+		}while(next_permutation(yoko.begin(),yoko.end()));
+	}while(next_permutation(tate.begin(),tate.end()));
+	if(ans==INF) cout<<-1<<endl;
+	else cout<<ans<<endl;
+	return 0;
+}
