51b3ee717940e338619f0fde0a2d6049c5651e7b
mono-1729 <kawai.172957@gmail.com>
2024-02-09 18:06:14 +0900

    20240209

---
+++ ABC/254/g.cpp
@@ -0,0 +1,432 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+//constexpr ll MOD = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+    ll val;
+    static vector<modint<MOD>> factorial_vec;
+public:
+    ll get() const { return (ll)val; }
+    // コンストラクタ
+    modint(ll x = 0){
+        val = x % MOD;
+        if(val < 0) x += MOD;
+    }
+
+    // 入出力ストリーム
+    friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+        ll y; is >> y;
+        x = y;
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+        return os << x.val;
+    }
+
+    // 算術演算子
+    modint<MOD> operator -(){return modint<MOD>(-val);}
+    modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+    modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+    modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+    modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+    // 代入演算子
+    modint<MOD> &operator +=(const modint<MOD> &r){
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    modint<MOD> &operator -=(const modint<MOD> &r){
+        if(val < r.val) val += MOD;
+        val -= r.val;
+        return *this;
+    }
+    modint<MOD> &operator *=(const modint<MOD> &r){
+        val = val*r.val%MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    modint<MOD> &operator /=(const modint<MOD> &r){
+        *this *= inv(r);
+        return *this;
+    }
+
+    //等価比較演算子
+    bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+    bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+    bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+    bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+    bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+    bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+    // 累乗
+    static modint<MOD> modpow(modint<MOD> num, ll exp){
+        if(!exp) return modint<MOD>(1); // 0乗
+        modint<MOD> ret(1);
+        modint<MOD> tmp = num;
+        while(exp){
+            if(exp&1) ret *= tmp;
+            tmp *= tmp;
+            exp >>= 1;
+        }
+        return ret;
+    }
+
+    // 逆元
+    static modint<MOD> inv(modint<MOD> num){
+        return modpow(num, MOD-2);
+    }
+
+    // 階乗
+    static modint<MOD> factorial(ll n){
+        modint<MOD> ret(1);
+        if(n == 0) return ret;
+        if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+        ret = factorial(n-1)*n;
+        factorial_vec.push_back(ret);
+        return ret;
+    }
+
+    // コンビネーション
+    static modint<MOD> combination(ll n, ll r){
+        return factorial(n) / factorial(r) / factorial(n-r);
+    }
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+ll gcd(ll a, ll b){
+    if(a%b == 0){
+      return b;
+    }else{
+      return gcd(b, a%b);
+    }
+}
+
+ll lcm(ll a, ll b){
+    return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+    if (n == 0) return 1 % MOD;
+    ll val = powMod(x, n / 2);
+    val *= val;
+    val %= MOD;
+    if (n % 2 == 1) val *= x;
+    return val % MOD;
+}
+
+int getrandmax() {
+    static uint32_t y = time(NULL);
+    y ^= (y << 13); y ^= (y >> 17);
+    y ^= (y << 5);
+    return abs((int)y);
+}
+// [l, r]
+int getrand(int l, int r) {
+    return getrandmax() % (r - l + 1) + l;
+}
+typedef modint<1000000007> mint1;
+typedef modint<1000000009> mint2;
+int primes[10] = { 10007, 10009, 10037, 10039, 10061, 10067, 10069, 10079, 10091, 10093 };
+bool isShuffle = false;
+struct RollingHash {
+    mint1 p1; mint2 p2;
+    int n;
+    vector<mint1> m1; vector<mint2> m2;
+    vector<mint1> v1; vector<mint2> v2;
+    vector<mint1> r1; vector<mint2> r2;
+
+    RollingHash() {
+        if (!isShuffle) {
+            rep(i, 0, 101) { int a = getrand(0, 9); int b = getrand(0, 9); swap(primes[a], primes[b]); }
+            isShuffle = true;
+        }
+        p1 = primes[0], p2 = primes[1];
+    }
+
+    void init(string s, char o = 'a') {
+        vector<int> v;
+        for(auto c: s) v.push_back(c - o + 1);
+        init(v);
+    }
+
+    void init(vector<int> s) {
+        n = s.size();
+        m1.resize(n); m2.resize(n); v1.resize(n); v2.resize(n); r1.resize(n); r2.resize(n);
+
+        m1[0] = 1; m2[0] = 1;
+        v1[0] = s[0]; v2[0] = s[0];
+
+        rep(i, 1, n) {
+            m1[i] = m1[i - 1] * p1;
+            m2[i] = m2[i - 1] * p2;
+            v1[i] = v1[i - 1] + m1[i] * s[i];
+            v2[i] = v2[i - 1] + m2[i] * s[i];
+        }
+
+        r1[n - 1] = mint1(1) / m1[n - 1];
+        rrep(i, n - 2, 0) r1[i] = r1[i + 1] * p1;
+
+        r2[n - 1] = mint2(1) / m2[n - 1];
+        rrep(i, n - 2, 0) r2[i] = r2[i + 1] * p2;
+    }
+    // s[l..r]
+    inline pair<mint1, mint2> hash(int l, int r) {
+        if (l > r) return { 0,0 };
+        assert(l <= r); assert(r < n);
+        mint1 a = v1[r];
+        if (l) a -= v1[l - 1];
+        a *= r1[l];
+
+        mint2 b = v2[r];
+        if (l) b -= v2[l - 1];
+        b *= r2[l];
+
+        return { a, b };
+    }
+    // s[l..r]
+	inline ll llhash(int l, int r) {
+		auto h = hash(l, r);
+		return (ll)h.first.get() * (ll)1000000009 + (ll)h.second.get();
+	}
+};
+
+template <class T, T (*op)(T, T), T (*e)(), class F, T (*mapping)(F, T), F (*composition)(F, F), F (*id)()> 
+class LazySegmentTree {
+    ll _n, size, log;
+    vector<T> d;
+    vector<F> lz;
+
+    void update(ll k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+
+    void all_apply(ll k, F f){
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+
+    void push(ll k){
+        all_apply(2*k, lz[k]);
+        all_apply(2*k+1, lz[k]);
+        lz[k] = id();
+    }
+
+public:
+    LazySegmentTree() : LazySegmentTree(0) {}
+    explicit LazySegmentTree(ll n) : LazySegmentTree(vector<T>(n, e())) {} // explicit で明示的に型を指定する
+    explicit LazySegmentTree(const vector<T> &v) : _n(int(v.size())) {
+        // sizeは_nを超える最小の2のべき乗
+        size = 1;
+        while(size < _n) size *= 2, log++; 
+
+        // log は木の高さ（sizeの桁数）
+        log = 0;
+        while (!(size & (1 << log))) log++;
+
+        d = vector<T>(2*size, e());
+        lz = vector<F>(size, id());
+
+        for(ll i = 0; i < _n; i++) d[size+i] = v[i];
+        for(ll i = size-1; i >= 1; i--){
+            update(i);
+        }
+    }
+
+    void set(ll p, T x){
+        assert(0 <= p && p < _n);
+        p += size;
+        for(ll i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for(ll i = 1; i <= log; i++) update(p >> i);
+    }
+
+    T get(ll p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for(ll i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+
+
+    T prod(ll l, ll r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if(l == r) return e();
+
+        l += size;
+        r += size;
+
+        for(ll i = log; i >= 1; i--){
+            if(((l >> i) << i) != l) push(l >> i);
+            if(((r >> i) << i) != r) push((r-1) >> i);
+        }
+        
+        T sml = e(), smr = e();
+        while(l < r){
+            if(l&1) sml = op(sml, d[l++]);
+            if(r&1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    T all_prod() {return d[1]; }
+
+    void apply(ll p, F f){
+        assert(0 <= p && p < _n);
+        p += size;
+        for(ll i = log; i >= 1; i--) push(p >> i); 
+        d[p] = mapping(f, d[p]);
+        for(ll i = 1; i <= log; i++) update(p >> i);
+    }
+
+    void apply(ll l, ll r, F f){
+        assert(0 <= l && l <= r && r <= _n);
+        if(l == r) return;
+
+        l += size;
+        r += size;
+
+        for(ll i = log; i >= 1; i--){
+            if(((l >> i) << i) != l) push(l >> i);
+            if(((r >> i) << i) != r) push((r-1) >> i);
+        }
+
+        {
+            ll l2 = l, r2 = r;
+            while(l < r){
+                if(l&1) all_apply(l++, f);
+                if(r&1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for(ll i = 1; i <= log; i++){
+            if(((l >> i) << i) != l) update(l >> i);
+            if(((r >> i) << i) != r) update((r-1) >> i);
+        }
+    }
+
+    // f(op(a[l], a[l + 1], ..., a[r - 1])) = trueとなる最大のｒ
+    template <bool (*g)(T)> ll max_right(ll l) {
+        return max_right(l, [](T x) { return g(x); });
+    }
+    template <class G> ll max_right(ll l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (ll i = log; i >= 1; i--) push(l >> i);
+        T sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*g)(T)> ll min_left(ll r) {
+        return min_left(r, [](T x) { return g(x); });
+    }
+    template <class G> ll min_left(ll r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (ll i = log; i >= 1; i--) push((r - 1) >> i);
+        T sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+};
+
+//区間変更・区間最小値取得
+using S = long long;
+using F = long long;
+const F ID = 8e18;
+
+S op(S a, S b){ return std::min(a, b); }
+S e(){ return INF; }
+S mapping(F f, S x){ return (f == ID ? x : min(x,f)); }
+F composition(F f, F g){ return (f == ID ? g : min(f,g)); }
+F id(){ return ID; }
+// ll n;
+// vector<S> v(n);
+// LazySegmentTree<S, op, e, F, mapping, composition, id> seg(v);
+
+int main() {
+    string s,t;cin>>s>>t;
+    RollingHash sh,th;
+    sh.init(s);
+    th.init(t);
+    vector<ll> v(t.size()+1,INF);
+    v[0]=0;
+    LazySegmentTree<S, op, e, F, mapping, composition, id> seg(v);
+    rep(i,0,t.size()){
+        ll l=0,r=t.size()-i+1;
+        while(r-l>1){
+            ll m=(l+r)/2;
+            if(m<=s.size() && sh.llhash(0,m-1)==th.llhash(i,i+m-1)) l=m;
+            else r=m;
+        }
+        seg.apply(i+1,i+l+1,i);
+    }
+    ll ans=0;
+    ll now=t.size();
+    while(now>0){
+        ll next=seg.get(now);
+        if(next==INF || next==now){
+            cout<<-1<<endl;
+            return 0;
+        }
+        now=next;
+        ans++;
+    }
+    cout<<ans<<endl;
+    // rep(i,0,t.size()+1)cout<<seg.get(i)<<" ";
+    return 0;
+}
