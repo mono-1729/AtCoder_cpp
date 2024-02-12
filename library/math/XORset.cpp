d4382c8f7f8014826b326349ee9d8a6c42949bcf
mono-1729 <kawai.172957@gmail.com>
2024-02-13 00:59:17 +0900

    20230212

---
+++ library/math/XORset.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+class xor_set{
+private:
+    vector<ll> w;
+public:
+    xor_set () {}
+    void insert(ll x){
+        for(ll v : w) if(v & -v & x) x ^= v;
+        if(x == 0) return;
+        for(ll& v : w) if(x & -x & v) v ^= x;
+        w.push_back(x);
+    }
+ 
+    ll count(ll x){
+        for(ll v : w) if(v & -v & x) x ^= v;
+        if(x == 0) return 1;
+        else return 0;
+    }
+};
+
+int main() {
+    return 0;
+}
