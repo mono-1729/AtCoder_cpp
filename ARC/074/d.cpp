bca835246e1e55b8922a0502b57b3d961010ee67
mono-1729 <kawai.172957@gmail.com>
2024-03-24 13:25:04 +0900

    20240324

---
+++ ARC/074/d.cpp
@@ -0,0 +1,226 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+//constexpr ll MOD = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+    ll val;
+    static vector<modint<MOD>> factorial_vec;
+public:
+    ll get() const { return (ll)val; }
+    // コンストラクタ
+    modint(ll x = 0){
+        val = x % MOD;
+        if(val < 0) x += MOD;
+    }
+
+    // 入出力ストリーム
+    friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+        ll y; is >> y;
+        x = y;
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+        return os << x.val;
+    }
+
+    // 算術演算子
+    modint<MOD> operator -(){return modint<MOD>(-val);}
+    modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+    modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+    modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+    modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+    // 代入演算子
+    modint<MOD> &operator +=(const modint<MOD> &r){
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    modint<MOD> &operator -=(const modint<MOD> &r){
+        if(val < r.val) val += MOD;
+        val -= r.val;
+        return *this;
+    }
+    modint<MOD> &operator *=(const modint<MOD> &r){
+        val = val*r.val%MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    modint<MOD> &operator /=(const modint<MOD> &r){
+        *this *= inv(r);
+        return *this;
+    }
+
+    //等価比較演算子
+    bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+    bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+    bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+    bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+    bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+    bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+    // 累乗
+    static modint<MOD> modpow(modint<MOD> num, ll exp){
+        if(!exp) return modint<MOD>(1); // 0乗
+        modint<MOD> ret(1);
+        modint<MOD> tmp = num;
+        while(exp){
+            if(exp&1) ret *= tmp;
+            tmp *= tmp;
+            exp >>= 1;
+        }
+        return ret;
+    }
+
+    // 逆元
+    static modint<MOD> inv(modint<MOD> num){
+        return modpow(num, MOD-2);
+    }
+
+    // 階乗
+    static modint<MOD> factorial(ll n){
+        modint<MOD> ret(1);
+        if(n == 0) return ret;
+        if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+        ret = factorial(n-1)*n;
+        factorial_vec.push_back(ret);
+        return ret;
+    }
+
+    // コンビネーション
+    static modint<MOD> combination(ll n, ll r){
+        return factorial(n) / factorial(r) / factorial(n-r);
+    }
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+ll gcd(ll a, ll b){
+    if(a%b == 0){
+      return b;
+    }else{
+      return gcd(b, a%b);
+    }
+}
+
+ll lcm(ll a, ll b){
+    return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+    if (n == 0) return 1 % MOD;
+    ll val = powMod(x, n / 2);
+    val *= val;
+    val %= MOD;
+    if (n % 2 == 1) val *= x;
+    return val % MOD;
+}
+
+struct edge {
+	ll to, cap, rev;
+	edge(ll to, ll cap, ll rev) : to(to), cap(cap), rev(rev) {}
+};
+
+const ll MAX_V = 30000;
+
+vector<edge> g[MAX_V];
+ll level[MAX_V];
+ll iter[MAX_V];
+
+void add_edge(ll from, ll to, ll cap) {
+	g[from].emplace_back(to, cap, g[to].size());
+	g[to].emplace_back(from, 0, g[from].size() - 1);
+}
+
+void bfs(ll s) {
+	memset(level, -1, sizeof(level));
+	queue<ll> que;
+	level[s] = 0;
+	que.push(s);
+	while(!que.empty()) {
+		ll v = que.front(); que.pop();
+		for(auto &e : g[v]) {
+			if(e.cap > 0 && level[e.to] < 0) {
+				level[e.to] = level[v] + 1;
+				que.push(e.to);
+			}
+		}
+	}
+}
+
+ll dfs(ll v, ll t, ll f) {
+	if(v == t) return f;
+	for(ll &i = iter[v]; i < g[v].size(); i++) {
+		edge &e = g[v][i];
+		if(e.cap > 0 && level[v] < level[e.to]) {
+			ll d = dfs(e.to, t, min(f, e.cap));
+			if(d > 0) {
+				e.cap -= d;
+				g[e.to][e.rev].cap += d;
+				return d;
+			}
+		}
+	}
+	return 0;
+}
+
+ll max_flow(ll s, ll t) {
+	ll flow = 0;
+	while(true) {
+		bfs(s);
+		if(level[t] < 0) return flow;
+		memset(iter, 0, sizeof(iter));
+		ll f;
+		while((f = dfs(s, t, 1e18)) > 0) {
+			flow += f;
+		}
+	}
+}
+
+int main() {
+    ll h,w;cin>>h>>w;
+    vector<string> a(h);
+    ll si,sj,gi,gj;
+    rep(i,0,h) cin>>a[i];
+    rep(i,0,h) rep(j,0,w){
+        if(a[i][j] == 'S') si = i, sj = j;
+        if(a[i][j] == 'T') gi = i, gj = j;
+    }
+    if(si == gi || sj == gj){
+        cout<<-1<<endl;
+        return 0;
+    }
+    rep(i,0,h){
+        rep(j,0,w){
+            add_edge(i*w+j, i*w+j+10000, 1);
+            if(a[i][j] == '.') continue;
+            rep(ni,i+1,h){
+                if(a[ni][j] != '.'){
+                    add_edge(i*w+j+10000, ni*w+j, 1);
+                    add_edge(ni*w+j+10000, i*w+j, 1);
+                }
+            }
+            rep(nj,j+1,w){
+                if(a[i][nj] != '.'){
+                    add_edge(i*w+j+10000, i*w+nj, 1);
+                    add_edge(i*w+nj+10000, i*w+j, 1);
+                }
+            }
+        }
+    }
+    cout<<max_flow(si*w+sj+10000, gi*w+gj)<<endl;
+    return 0;
+}
