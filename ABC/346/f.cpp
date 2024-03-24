3d983fddf6a7e7dc77cdca57b59bf7785238988e
mono-1729 <kawai.172957@gmail.com>
2024-03-24 13:05:15 +0900

    20240323

---
+++ ABC/346/f.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <stdlib.h>
+using namespace std;
+#define rep(i, a, n) for(ll i = a; i < n; i++)
+#define rrep(i, a, n) for(ll i = a; i >= n; i--)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+//constexpr ll MOD = 1000000007;
+constexpr ll MOD = 998244353;
+constexpr int IINF = 1001001001;
+constexpr ll INF = 1LL<<60;
+template<class t,class u> void chmax(t&a,u b){if(a<b)a=b;}
+template<class t,class u> void chmin(t&a,u b){if(b<a)a=b;}
+template <ll MOD> class modint {
+    ll val;
+    static vector<modint<MOD>> factorial_vec;
+public:
+    ll get() const { return (ll)val; }
+    // コンストラクタ
+    modint(ll x = 0){
+        val = x % MOD;
+        if(val < 0) x += MOD;
+    }
+
+    // 入出力ストリーム
+    friend constexpr istream &operator>>(istream &is, modint<MOD> &x){
+        ll y; is >> y;
+        x = y;
+        return is;
+    }
+    friend constexpr ostream &operator<<(ostream &os, const modint<MOD> &x){
+        return os << x.val;
+    }
+
+    // 算術演算子
+    modint<MOD> operator -(){return modint<MOD>(-val);}
+    modint<MOD> operator +(const modint<MOD> &r) const { return modint<MOD>(*this) += r; }
+    modint<MOD> operator -(const modint<MOD> &r) const { return modint<MOD>(*this) -= r; }
+    modint<MOD> operator *(const modint<MOD> &r) const { return modint<MOD>(*this) *= r; }
+    modint<MOD> operator /(const modint<MOD> &r) const { return modint<MOD>(*this) /= r; }
+
+    // 代入演算子
+    modint<MOD> &operator +=(const modint<MOD> &r){
+        val += r.val;
+        if(val >= MOD) val -= MOD;
+        return *this;
+    }
+    modint<MOD> &operator -=(const modint<MOD> &r){
+        if(val < r.val) val += MOD;
+        val -= r.val;
+        return *this;
+    }
+    modint<MOD> &operator *=(const modint<MOD> &r){
+        val = val*r.val%MOD;
+        if(val < 0) val += MOD;
+        return *this;
+    }
+    modint<MOD> &operator /=(const modint<MOD> &r){
+        *this *= inv(r);
+        return *this;
+    }
+
+    //等価比較演算子
+    bool operator ==(const modint<MOD>& r){return this -> val == r.val;}
+    bool operator !=(const modint<MOD>& r){return this -> val != r.val;}
+    bool operator <(const modint<MOD>& r){return this -> val < r.val;}
+    bool operator <=(const modint<MOD>& r){return this -> val <= r.val;}
+    bool operator >(const modint<MOD>& r){return this -> val > r.val;}
+    bool operator >=(const modint<MOD>& r){return this -> val >= r.val;}
+
+    // 累乗
+    static modint<MOD> modpow(modint<MOD> num, ll exp){
+        if(!exp) return modint<MOD>(1); // 0乗
+        modint<MOD> ret(1);
+        modint<MOD> tmp = num;
+        while(exp){
+            if(exp&1) ret *= tmp;
+            tmp *= tmp;
+            exp >>= 1;
+        }
+        return ret;
+    }
+
+    // 逆元
+    static modint<MOD> inv(modint<MOD> num){
+        return modpow(num, MOD-2);
+    }
+
+    // 階乗
+    static modint<MOD> factorial(ll n){
+        modint<MOD> ret(1);
+        if(n == 0) return ret;
+        if((ll)factorial_vec.size() >= n) return factorial_vec[n-1];
+        ret = factorial(n-1)*n;
+        factorial_vec.push_back(ret);
+        return ret;
+    }
+
+    // コンビネーション
+    static modint<MOD> combination(ll n, ll r){
+        return factorial(n) / factorial(r) / factorial(n-r);
+    }
+
+};
+
+using mint = modint<MOD>;
+template <ll MOD> vector<modint<MOD>> modint<MOD>::factorial_vec;
+
+ll gcd(ll a, ll b){
+    if(a%b == 0){
+      return b;
+    }else{
+      return gcd(b, a%b);
+    }
+}
+
+ll lcm(ll a, ll b){
+    return a*b / gcd(a, b);
+}
+
+ll powMod(ll x, ll n) {
+    if (n == 0) return 1 % MOD;
+    ll val = powMod(x, n / 2);
+    val *= val;
+    val %= MOD;
+    if (n % 2 == 1) val *= x;
+    return val % MOD;
+}
+
+int main() {
+    ll n;cin>>n;
+    string s,t;cin>>s>>t;
+    vector<vector<ll>> count(26,vector<ll>(s.size(),0));
+    rep(i,0,s.size())count[s[i]-'a'][i]++;
+    rep(i,0,26)rep(j,1,s.size())count[i][j] += count[i][j-1];
+    rep(i,0,t.size())if(count[t[i]-'a'][s.size()-1] == 0){
+        cout << 0 << endl;
+        return 0;
+    }
+    ll ok = 0, ng = 2e17/t.size();
+    while(ng-ok>1){
+        ll mid = (ok+ng)/2;
+        bool flag = true;
+        ll cnt = -1, idx = s.size()-1;
+        rep(i,0,t.size()){
+            ll x = mid-1;
+            cnt+=x/count[t[i]-'a'][s.size()-1];
+            x%=count[t[i]-'a'][s.size()-1];
+            x++;
+            if(count[t[i]-'a'][s.size()-1]-count[t[i]-'a'][idx]<x){
+                x -= count[t[i]-'a'][s.size()-1]-count[t[i]-'a'][idx];
+                cnt++;
+                idx = lower_bound(count[t[i]-'a'].begin(),count[t[i]-'a'].end(),x)-count[t[i]-'a'].begin();
+            }
+            else{
+                idx = lower_bound(count[t[i]-'a'].begin(),count[t[i]-'a'].end(),count[t[i]-'a'][idx]+x)-count[t[i]-'a'].begin();
+            }
+            if(cnt >= n){
+                //if(mid==2) cout<<i<<" "<<cnt<<" "<<idx<<endl;
+                flag = false;
+                break;
+            }
+        }
+        if(flag)ok = mid;
+        else ng = mid;
+    }
+    cout << ok << endl;
+    return 0;
+}
