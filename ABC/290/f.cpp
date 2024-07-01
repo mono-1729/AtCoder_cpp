eeb13454b367c1524330cfa5d51fd55416f932ca
mono-1729 <kawai.172957@gmail.com>
2024-07-01 14:05:13 +0900

    20240701

--- ABC/290/f.cpp
+++ ABC/290/f.cpp
@@ -41,10 +41,35 @@ ll powMod(ll x, ll n) {
     return val % MOD;
 }
 
+ll maxnum=2000005;
+vector<ll> fac(maxnum), inv(maxnum), finv(maxnum);
+void init_fac(){
+    fac[0] = fac[1] = 1;
+    inv[1] = 1;
+    finv[0] = finv[1] = 1;
+    rep(i, 2, maxnum){
+        fac[i] = fac[i-1]*i%MOD;
+        inv[i] = MOD-MOD/i*inv[MOD%i]%MOD;
+        finv[i] = finv[i-1]*inv[i]%MOD;
+    }
+}
+ll nCr(ll n, ll r){
+    if(n < 0 or n-r < 0 or r < 0) return 0;
+    return fac[n]*(finv[n-r]*finv[r]%MOD)%MOD;
+}
+ll nHr(ll n, ll r){
+    return nCr(n+r-1, r);
+}
+
 int main() {
+    init_fac();
     int t; cin >> t;
     while(t--){
-        
+        ll n; cin >> n;
+        mint ans = 0;
+        ans+=nCr(2*n-3,n-2);
+        ans+=nCr(2*n-4,n-3)*n;
+        cout << ans.val() << endl;
     }
     return 0;
 }
