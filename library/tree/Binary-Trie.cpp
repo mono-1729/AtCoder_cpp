88d5f2638c5c3f25d467cdfe1aa8f5427e5e00e7
mono-1729 <kawai.172957@gmail.com>
2026-09-13 14:19:24 +0900

    beam

--- library/tree/Binary-Trie.cpp
+++ library/tree/Binary-Trie.cpp
@@ -31,7 +31,7 @@ class binary_trie {
         if (!t) t = new node;
         t->cnt += 1;
         if (b < 0) return t;
-        bool f = (val >> (U)b) & (U)1;
+        bool f = ((val >> b) & (U)1) != 0;
         t->ch[f] = add(t->ch[f], val, b - 1);
         return t;
     }
@@ -40,24 +40,25 @@ class binary_trie {
         t->cnt -= 1;
         if (t->cnt == 0) return nullptr;
         if (b < 0) return t;
-        bool f = (val >> (U)b) & (U)1;
+        bool f = ((val >> b) & (U)1) != 0;
         t->ch[f] = sub(t->ch[f], val, b - 1);
         return t;
     }
     U get_min(node* t, U val, int b = B - 1) const {
         assert(t);
         if (b < 0) return 0;
-        bool f = (val >> (U)b) & (U)1; f ^= !t->ch[f];
-        return get_min(t->ch[f], val, b - 1) | ((U)f << (U)b);
+        bool f = ((val >> b) & (U)1) != 0;
+        f ^= !t->ch[f];
+        return get_min(t->ch[f], val, b - 1) | ((U)f << b);
     }
     U get(node* t, int k, int b = B - 1) const {
         if (b < 0) return 0;
         int m = t->ch[0] ? t->ch[0]->cnt : 0;
-        return k < m ? get(t->ch[0], k, b - 1) : get(t->ch[1], k - m, b - 1) | ((U)1 << (U)b);
+        return k < m ? get(t->ch[0], k, b - 1) : get(t->ch[1], k - m, b - 1) | ((U)1 << b);
     }
     int count_lower(node* t, U val, int b = B - 1) {
         if (!t || b < 0) return 0;
-        bool f = (val >> (U)b) & (U)1;
+        bool f = ((val >> b) & (U)1) != 0;
         return (f && t->ch[0] ? t->ch[0]->cnt : 0) + count_lower(t->ch[f], val, b - 1);
     }
     node *root;
@@ -95,7 +96,7 @@ public:
         if (!root) return 0;
         node *t = root;
         for (int i = B - 1; i >= 0; i--) {
-            t = t->ch[(val >> (U)i) & (U)1];
+            t = t->ch[((val >> i) & (U)1) != 0];
             if (!t) return 0;
         }
         return t->cnt;
